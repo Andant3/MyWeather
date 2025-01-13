14382ca75564b46b7283b9a41c9fe4971b3e2616
Andant3 <kamikadze3571@gmail.com>
2025-01-13 19:57:12 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import com.example.myweather.ui.theme.MyWeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyWeatherTheme {
+                Image(
+                    painter = painterResource(R.drawable.fog),
+                    contentDescription = "Image"
+                )
+            }
+        }
+    }
+}
